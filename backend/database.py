b3485e239cc9e9e6d5cd1580e697717068ba2a5f
prakash95150 <gnana9515@gmail.com>
2026-02-06 15:57:23 +0530

    fix: use render database url

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Prakash%40123@localhost:5432/wiki_quiz_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
